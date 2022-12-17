2f0009f7d3269556d2fcc34232a082397aac3e04
vibha-2601 <vibhanimbalkar2601@gmail.com>
2022-12-17 22:15:03 +0530

    Building Email-List

--- src/App.js
+++ src/App.js
@@ -3,14 +3,31 @@ import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
 
+// import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { Mail } from '@mui/icons-material';
+import EmailList from './EmailList';
+
+
+
 function App() {
   return (
+    <Router>
     <div className="app">
       <Header />
-      <Sidebar />
-        
-    </div>
+
+      <div className='app__body'>
+       <Sidebar />
+       
+       <Routes>
+        <Route path='/email' element={<Mail />} />
+        <Route path='/' element={<EmailList />} />
+      </Routes>
+       </div>
+      </div>
+    </Router>
   );
 }
-
 export default App;
+        
+    
